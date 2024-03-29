70340752d93cf4401a72b22a683f81301fb23cb0
Deivison Oliveira Costa <112001225+Deivison-Costa@users.noreply.github.com>
2024-03-29 15:24:19 -0300

    Update productRoutes.js

--- api-produto/routes/productRoutes.js
+++ api-produto/routes/productRoutes.js
@@ -3,20 +3,7 @@ const multer = require('multer');
 const router = express.Router();
 const productController = require('../controllers/productController');
 const authMiddleware = require('../middleware/authMiddleware');
-
-// configuração do multer
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads'); 
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname); 
-  }
-});
-
-// instancia o multer
-const upload = multer({ storage: storage });
-
+const upload = require('../configs/configUpload');
 
 // centraliza as rotas para produtos
 router.get('/', authMiddleware.authenticateToken, productController.getAllProducts);
