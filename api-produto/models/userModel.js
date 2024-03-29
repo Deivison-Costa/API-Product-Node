d91d4584a8a3f3a774764fab071df255e7617a70
Deivison Oliveira Costa <112001225+Deivison-Costa@users.noreply.github.com>
2024-03-29 16:49:50 -0300

    Update userModel.js

--- api-produto/models/userModel.js
+++ api-produto/models/userModel.js
@@ -16,7 +16,6 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
-// compara a senha com a armazenada no banco de dados
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
