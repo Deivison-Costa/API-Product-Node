609b88fdd5d9c6a646f1d0d4ee7beb45fda33fa8
Deivison Oliveira Costa <112001225+Deivison-Costa@users.noreply.github.com>
2024-03-29 18:32:45 -0300

    Update product.test.js

--- api-produto/tests/product.test.js
+++ api-produto/tests/product.test.js
@@ -7,7 +7,7 @@ const Product = require('../models/productModel');
 
 beforeAll(async () => {
   // conecta ao banco de dados antes de todos os testes e limpa as coleções
-  conn();
+  await conn();
   await User.deleteMany({});
   await Product.deleteMany({});
 });
@@ -15,6 +15,7 @@ beforeAll(async () => {
 describe('Testes da API', () => {
   let token;
   let productId; 
+
   // teste para registro de usuário
   it('Deve registrar um novo usuário', async () => {
     const response = await request(app)
@@ -22,6 +23,7 @@ describe('Testes da API', () => {
       .send({ username: 'usuario_teste', password: 'senha_teste' });
     expect(response.status).toBe(201);
   });  
+
   // teste para login de usuário
   it('Deve fazer login e retornar um token JWT', async () => {
     const response = await request(app)
@@ -30,8 +32,9 @@ describe('Testes da API', () => {
     expect(response.status).toBe(200);
     token = response.body.token;
   });
-  // teste para criar um novo produto
-  it('Deve criar um novo produto', async () => {
+
+  // teste para criar um novo produto 1
+  it('Deve criar um novo produto 1', async () => {
     const response = await request(app)
       .post('/products')
       .set('Authorization', `${token}`)
@@ -43,6 +46,20 @@ describe('Testes da API', () => {
     expect(response.status).toBe(201);
     productId = response.body._id;
   });
+
+  // teste para criar um novo produto 2
+  it('Deve criar um novo produto 2', async () => {
+    const response = await request(app)
+      .post('/products')
+      .set('Authorization', `${token}`)
+      .send({ 
+        name: 'Produto de Teste 2', 
+        description: 'Descrição do Produto de Teste 2', 
+        price: 29.99, 
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/76/Bola_de_futebol.jpg' });
+    expect(response.status).toBe(201);
+  });
+
   // teste para atualizar um produto existente
   it('Deve atualizar um produto existente', async () => {
     const response = await request(app)
@@ -51,13 +68,57 @@ describe('Testes da API', () => {
       .send({ name: 'Novo Nome', description: 'Nova Descrição', price: 29.99 });
     expect(response.status).toBe(200);
   });
+
   // teste para listar produtos com paginação
   it('Deve listar os produtos com paginação', async () => {
     const response = await request(app)
       .get('/products?page=1&limit=10')
       .set('Authorization', `${token}`);
     expect(response.status).toBe(200);
-    expect(response.body).toHaveLength(1); // nº esperado de produtos por página
+    expect(response.body).toHaveLength(2); // número esperado de produtos por página
+  });
+
+  // teste para validar se o preço do produto é um número positivo
+  it('Deve retornar erro ao criar um produto com preço negativo', async () => {
+    const response = await request(app)
+      .post('/products')
+      .set('Authorization', `${token}`)
+      .send({ 
+        name: 'Produto de Teste', 
+        description: 'Descrição do Produto de Teste', 
+        price: -10, 
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/76/Bola_de_futebol.jpg' });
+    expect(response.status).toBe(400);
+  });
+
+  // teste para buscar produtos pelo nome ou descrição
+  it('Deve buscar produtos pelo nome ou descrição', async () => {
+    const response = await request(app)
+      .get('/products?searchTerm=Produto')
+      .set('Authorization', `${token}`);
+    expect(response.status).toBe(200);
+    // verifica se o resultado da busca contém o produto criado anteriormente
+    expect(response.body.some(product => product.name === 'Produto de Teste 2')).toBeTruthy();
+  });
+
+  // teste para ordenar produtos pelo nome
+  it('Deve ordenar produtos pelo nome', async () => {
+    const response = await request(app)
+      .get('/products?sortBy=name&sortOrder=asc')
+      .set('Authorization', `${token}`);
+    expect(response.status).toBe(200);
+    // verifica se os produtos estão ordenados pelo nome em ordem ascendente
+    expect(response.body[0].name).toBe('Novo Nome');
+  });
+
+  // teste para ordenar produtos pelo preço
+  it('Deve ordenar produtos pelo preço', async () => {
+    const response = await request(app)
+      .get('/products?sortBy=price&sortOrder=asc')
+      .set('Authorization', `${token}`);
+    expect(response.status).toBe(200);
+    // verifica se os produtos estão ordenados pelo preço em ordem ascendente
+    expect(response.body[0].price).toBe(29.99);
   });
   // teste para excluir um produto
   it('Deve excluir um produto existente', async () => {
