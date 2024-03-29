7f711405ac718a6aa3b14762b8cea8ca74f00b86
Deivison Oliveira Costa <112001225+Deivison-Costa@users.noreply.github.com>
2024-03-29 16:08:41 -0300

    Update productController.js

--- api-produto/controllers/productController.js
+++ api-produto/controllers/productController.js
@@ -1,4 +1,6 @@
 const Product = require('../models/productModel');
+const NodeCache = require('node-cache');
+const cache = new NodeCache();
 
 // retorna todos os produtos com paginação
 exports.getAllProducts = async (req, res) => {
@@ -8,11 +10,19 @@ exports.getAllProducts = async (req, res) => {
   const sortBy = req.query.sortBy || '_id';
   const sortOrder = req.query.sortOrder || 'asc';
 
+  const cacheKey = `products_${page}_${limit}_${sortBy}_${sortOrder}`;
+  const cachedData = cache.get(cacheKey);
+
+  if (cachedData) {
+    return res.json(cachedData);
+  }
+
   const sortOptions = {};
   sortOptions[sortBy] = sortOrder === 'desc' ? -1 : 1;
 
   try {
     const products = await Product.find().skip(skip).limit(limit).sort(sortOptions);
+    cache.set(cacheKey, products);
     res.json(products);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -21,14 +31,16 @@ exports.getAllProducts = async (req, res) => {
 
 // cria um novo produto
 exports.createProduct = async (req, res) => {
-  const product = new Product({
-    name: req.body.name,
-    description: req.body.description,
-    price: req.body.price
-  });
-
   try {
+    const product = new Product({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price
+    });
+
     const newProduct = await product.save();
+    // Limpando o cache após criar um novo produto
+    cache.flushAll();
     res.status(201).json(newProduct);
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -39,6 +51,8 @@ exports.createProduct = async (req, res) => {
 exports.updateProduct = async (req, res) => {
   try {
     await Product.findByIdAndUpdate(req.params.id, req.body);
+    // limpa o cache após atualizar um produto
+    cache.flushAll();
     res.json({ message: 'Product updated successfully' });
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -49,13 +63,15 @@ exports.updateProduct = async (req, res) => {
 exports.deleteProduct = async (req, res) => {
   try {
     await Product.findByIdAndDelete(req.params.id);
+    // limpa a cache após deletar um produto
+    cache.flushAll();
     res.json({ message: 'Product deleted successfully' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
 
-// upa uma imagem
+// upload de imagens
 exports.uploadImage = (req, res) => {
   const imageUrl = req.protocol + '://' + req.get('host') + '/uploads/' + req.file.filename;
   res.json({ imageUrl });
