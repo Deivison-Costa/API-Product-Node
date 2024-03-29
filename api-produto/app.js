c396f676c5b8a8917f5400278b3d17be9dbf99c8
Deivison Oliveira Costa <112001225+Deivison-Costa@users.noreply.github.com>
2024-03-29 17:00:40 -0300

    Update app.js

--- api-produto/app.js
+++ api-produto/app.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const conn = require('./db/conn');  
 const bodyParser = require('body-parser');
-const upload = require('./configs/configUpload');
 const productRoutes = require('./routes/productRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(bodyParser.json());
 
-// conecta ao mongoDB
+// conecta no mongoDB
 conn();
 
-// rotas
-app.post('/upload', upload.single('image'), (req, res) => {
-  const imageUrl = req.protocol + '://' + req.get('host') + '/uploads/' + req.file.filename;
-  res.json({ imageUrl });
-});
+// Rotas
 app.use('/products', productRoutes);
 app.use('/auth', authRoutes);
 
