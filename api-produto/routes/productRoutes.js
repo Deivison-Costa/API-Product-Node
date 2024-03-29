d82755b207a55af98e96732ef05133f4896b3707
Deivison Oliveira Costa <112001225+Deivison-Costa@users.noreply.github.com>
2024-03-29 17:01:11 -0300

    Update productRoutes.js

--- api-produto/routes/productRoutes.js
+++ api-produto/routes/productRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const multer = require('multer');
 const router = express.Router();
 const productController = require('../controllers/productController');
 const authMiddleware = require('../middleware/authMiddleware');
 const upload = require('../configs/configUpload');
 
-// centraliza as rotas para produtos
+// rota de upload
+router.post('/upload', authMiddleware.authenticateToken, upload.single('image'), productController.uploadImage);
+
+// rotas para produtos
 router.get('/', authMiddleware.authenticateToken, productController.getAllProducts);
 router.post('/', authMiddleware.authenticateToken, productController.createProduct);
 router.put('/:id', authMiddleware.authenticateToken, productController.updateProduct);
 router.delete('/:id', authMiddleware.authenticateToken, productController.deleteProduct);
-router.post('/upload', upload.single('image'), productController.uploadImage);
 
 module.exports = router;
