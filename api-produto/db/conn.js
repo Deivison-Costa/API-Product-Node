38c58afe59c4916f13538aaad73db99fad4597cd
Deivison Oliveira Costa <112001225+Deivison-Costa@users.noreply.github.com>
2024-03-29 19:18:01 -0300

    Update conn.js

--- api-produto/db/conn.js
+++ api-produto/db/conn.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const mongoURL = process.env.MONGODB_URI;
+
 // conecta ao mongoDB
 async function main() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(mongoURL);
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Could not connect to MongoDB', err);
